30b62265b693507a24d345a3a3b797a91e26e579
ThreeT <nii@noble24.pro>
2021-05-21 15:26:59 +0300

    функция получения данных из источника

---
+++ rates.py
@@ -0,0 +1,24 @@
+from bs4 import BeautifulSoup
+import requests
+
+def fetch_bitcoin():
+    url = "https://www.coingecko.com/en/coins/bitcoin"
+    headers = {'User-Agent' : 'Mozilla/5.0'}
+    bitcoin_file = requests.get(url)
+
+    # Создаём soup-объект
+    soup = BeautifulSoup(bitcoin_file.text, "html.parser")
+
+    bitcoin_li = []
+
+    # Извлекаем необходимые данные из тегов
+    for table in soup.find_all("table", attrs={"class" : "table b-b"}):
+        for td in table.find_all("td"):
+            bitcoin_li.append(td.text)
+
+    del bitcoin_li[3:]
+
+    # Убираем ненужные символы из элементов списка
+    bitcoin_li = map(lambda s : s.strip(), bitcoin_li)
+
+    return bitcoin_li
